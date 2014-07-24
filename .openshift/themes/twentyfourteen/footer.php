4f596bf0406e6bcabe561ae5fab0de37c962903e
ronak <ronak@rkmallik.com>
2014-07-24 22:42:52 +1000

    removing wordpress branding

--- .openshift/themes/twentyfourteen/footer.php
+++ .openshift/themes/twentyfourteen/footer.php
@@ -16,10 +16,6 @@
 
 			<?php get_sidebar( 'footer' ); ?>
 
-			<div class="site-info">
-				<?php do_action( 'twentyfourteen_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyfourteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfourteen' ), 'WordPress' ); ?></a>
-			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
 	</div><!-- #page -->
 
